93d6d1ddd30a578966546ffb14adec957b0a1c77
Ejaz Alam <alamejaz72@gmail.com>
2026-04-19 21:25:46 +0500

    fixes

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const startServer = async () => {
         });
         app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
     } catch (error) {
-        console.log("Error starting server", error);
+        console.log("Error starting server in index.js", error);
         process.exit(1);
     }
 }   
